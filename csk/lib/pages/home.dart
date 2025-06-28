651fb4ec76161aa2955f0a422aa89c13711215e7
Yvonne Wangui <wanguiyvonne333@gmail.com>
2025-06-28 12:22:12 +0300

    added profile image and updated pubspec.yaml

--- csk/lib/pages/home.dart
+++ csk/lib/pages/home.dart
@@ -19,7 +19,7 @@ class HomePage extends StatelessWidget {
           ),
         ),
         leading: CircleAvatar(
-          backgroundImage: AssetImage('assets/images/profile.jpg'),
+          backgroundImage: AssetImage('images/profile.jpg'),
          radius: 20,
         ),
       ),
