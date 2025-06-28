b0809492b1bb903d9073872fbfe625dbb3a8678d
Yvonne Wangui <wanguiyvonne333@gmail.com>
2025-06-28 11:45:52 +0300

    made changes to the following files: // csk/lib/main.dart // csk/lib/pages/home.dart // csk/lib/widgets/bottomnavigationbar.dart

--- csk/lib/widgets/bottomnavigationbar.dart
+++ csk/lib/widgets/bottomnavigationbar.dart
@@ -5,6 +5,15 @@ class MyBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Learn'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Network'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Profile'),
+        ],
+      ),
+    );
   }
 }
