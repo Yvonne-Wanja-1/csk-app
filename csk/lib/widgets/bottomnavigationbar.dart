21be9cd569e284733078693fafed17128800e8f1
Yvonne Wangui <wanguiyvonne333@gmail.com>
2025-06-28 11:51:15 +0300

    added color to the bottom navigation bar

--- csk/lib/widgets/bottomnavigationbar.dart
+++ csk/lib/widgets/bottomnavigationbar.dart
@@ -7,11 +7,11 @@ class MyBottomNavigationBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Learn'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Network'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Profile'),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home, color: Color(0xFF61738A)), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.school, color: Color(0xFF61738A)), label: 'Learn'),
+          BottomNavigationBarItem(icon: Icon(Icons.group, color: Color(0xFF61738A)), label: 'Network'),
+          BottomNavigationBarItem(icon: Icon(Icons.person, color: Color(0xFF61738A)), label: 'Profile'),
         ],
       ),
     );
