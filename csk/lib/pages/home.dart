033e1b2242db720af690818898d7d1e1ea662f2c
Yvonne Wangui <wanguiyvonne333@gmail.com>
2025-06-28 12:36:06 +0300

    added actions to the appbar

--- csk/lib/pages/home.dart
+++ csk/lib/pages/home.dart
@@ -7,10 +7,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //app bar
       appBar: AppBar(
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         elevation: 0,
         centerTitle: true,
+        //title
         title: const Text('Hey Nicholas 👋',
           style: TextStyle(
             color: Colors.black,
@@ -18,6 +20,7 @@ class HomePage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+        //leading
         leading: Padding(
           padding: const EdgeInsets.only(left: 22.0),
           child: CircleAvatar(
@@ -25,6 +28,18 @@ class HomePage extends StatelessWidget {
            radius: 25,
           ),
         ),
+        //actions
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 22.0),
+            child: IconButton(
+              icon: const Icon(Icons.notifications_active_outlined, color: Colors.black, size: 30),
+              onPressed: () {
+                // Handle notification button press
+              },
+            ),
+          ),
+        ],
       ),
       //body
       body: const Center(
