4a4dbca2f2644a39fc5ddf0fbb5dbbee1e76faa4
Yvonne Wangui <wanguiyvonne333@gmail.com>
2025-06-29 10:50:18 +0300

    made some changes to home.dart

--- csk/lib/pages/home.dart
+++ csk/lib/pages/home.dart
@@ -6,48 +6,65 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //app bar
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        elevation: 0,
-        centerTitle: true,
-        //title
-        title: const Text('Hey Nicholas 👋',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        //leading
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 22.0),
-          child: CircleAvatar(
-            backgroundImage: AssetImage('images/profile.jpg'),
-           radius: 25,
-          ),
-        ),
-        //actions
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 22.0),
-            child: IconButton(
-              icon: const Icon(Icons.notifications_active_outlined, color: Colors.black, size: 30),
-              onPressed: () {
-                // Handle notification button press
-              },
+    return SafeArea(
+      child: Scaffold(
+        //app bar
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          elevation: 0,
+          centerTitle: true,
+          //title
+          title: const Text('Hey Nicholas 👋',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        ],
+          //leading
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 22.0),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('images/profile.jpg'),
+             radius: 25,
+            ),
+          ),
+          //actions
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 22.0),
+              child: IconButton(
+                icon: const Icon(Icons.notifications_active_outlined, color: Colors.black, size: 30),
+                onPressed: () {
+                  // Handle notification button press
+                },
+              ),
+            ),
+          ],
+        ),
+        //body
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 32.0, top: 20.0, bottom: 8.0),
+                child: Text(
+                  'Your Track',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              ],
+          ),
+        ),
+      //bottom navigation bar
+      bottomNavigationBar: MyBottomNavigationBar(),
+      
       ),
-      //body
-      body: const Center(
-        child: Text('Welcome to the Home Page!'),
-      ),
-//bottom navigation bar
-bottomNavigationBar: MyBottomNavigationBar(),
-
     );
   }
 }
